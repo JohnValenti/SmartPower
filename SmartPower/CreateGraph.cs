ab7b9d3caea4f5e2d959e7b04e9a190a33a789e1
John Valenti <john.valenti@verint.com>
2017-08-04 11:42:22 +0100

    base project

---
+++ SmartPower/CreateGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPower
+{
+    class CreateGraph
+    {
+        ScheduleData schedule;
+
+        public CreateGraph (ScheduleData sd)
+        {
+            this.schedule = sd;
+        }
+
+        public void GraphData()
+        {
+            //do stuff to graph the schedule data and energy data
+        }
+    }
+}
---
+++ SmartPower/ScheduleReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPower
+{
+    class ScheduleReader : IFileReader
+    {
+        public string getFile()
+        {
+            return "filename.xml";
+        }
+
+        public void ParseFile()
+        {
+            //do stuff
+            //parse the data
+            //put into schedule data class
+        }
+    }
+}
